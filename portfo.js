335c76c6c6f342849241442a7e535e9c7e7d030b
Hamzattech <ridwanulllah@gmail.com>
2020-06-08 19:03:00 +0100

    arrow

--- portfo.js
+++ portfo.js
@@ -8,15 +8,9 @@ toggleButton.addEventListener('click', ()=>{
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollDown()};
+const scrollDown = ()=>(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ? mybutton.style.display = "block" : mybutton.style.display = "none");
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
