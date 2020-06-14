d0c58d6643dab9fe574493a27ae2037a81ced70e
Hamzattech <ridwanulllah@gmail.com>
2020-06-14 18:59:14 +0100

    newImagee

--- portfo.js
+++ portfo.js
@@ -2,14 +2,15 @@ const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', ()=>{
-    navbarLinks.classList.toggle('active')
+    navbarLinks.classList.toggle('active');
 })
 //Get the button:
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollDown()};
-const scrollDown = ()=>(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ? mybutton.style.display = "block" : mybutton.style.display = "none");
+const scrollDown = ()=>(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ? 
+  mybutton.style.display = "block" : mybutton.style.display = "none");
 
 
 // When the user clicks on the button, scroll to the top of the document
