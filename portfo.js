844360e64e236a0d8827c16dfd6f9755e15276b2
Hamzattech <ridwanulllah@gmail.com>
2020-06-23 22:02:41 +0100

    odd

--- portfo.js
+++ portfo.js
@@ -3,6 +3,7 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', ()=>{
     navbarLinks.classList.toggle('active');
+    
 })
 //Get the button:
 mybutton = document.getElementById("myBtn");
@@ -10,7 +11,7 @@ mybutton = document.getElementById("myBtn");
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollDown()};
 const scrollDown = ()=>(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ? 
-  mybutton.style.display = "inline" : mybutton.style.display = "none");
+  mybutton.style.display = "block" : mybutton.style.display = "none");
 
 
 // When the user clicks on the button, scroll to the top of the document
@@ -18,9 +19,5 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-$("document").ready(function(){
-  $("#xperience").mouseenter(function(){
-  $(".xp").slideDown(8000);
-});
+ 
 
-});
