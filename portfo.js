bc012ec72b2ca9cd903626c24798f73d4415a319
Hamzattech <ridwanulllah@gmail.com>
2020-06-29 04:12:52 +0100

    awesome stuffs

--- portfo.js
+++ portfo.js
@@ -4,20 +4,52 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', ()=>{
     navbarLinks.classList.toggle('active');
     
-})
-//Get the button:
-mybutton = document.getElementById("myBtn");
+});
+//get the button from html
+myButton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollDown()};
-const scrollDown = ()=>(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ? 
-  mybutton.style.display = "block" : mybutton.style.display = "none");
+//window.onscroll function to call the scroll function as soon as the user scroll down 20px from top
+window.onscroll=()=>{scroll()};
 
+//the scrolldown function
+const scroll = ()=>{document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ? myButton.style.display = 'block':
+   myButton.style.display = 'none';}
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+   //topfunction
+const topFunction = ()=>{document.body.scrollTop = 0;document.documentElement.scrollTop = 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  
 
